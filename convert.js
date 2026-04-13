1b1b59b99c9793e5f377871940826ec6da626e83
hupnpn <89003940+hupnpn@users.noreply.github.com>
2026-04-13 20:39:39 +0800

    Update convert.js

--- convert.js
+++ convert.js
@@ -576,18 +576,8 @@ function buildProxyGroups({
             name: PROXY_GROUPS.SELECT,
             icon: "https://gcore.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Global.png",
             type: "select",
-            proxies:[
-                    "香港节点", 
-                    "台湾节点", 
-                    "日本节点", 
-                    "韩国节点", 
-                    "新加坡节点", 
-                    "美国节点", 
-                    "手动选择", 
-                    "故障转移",
-                    "DIRECT"
-                    ]
-        },
+            proxies:[ "香港节点", "台湾节点", "日本节点", "韩国节点", "新加坡节点", "美国节点", "手动选择", "故障转移", "DIRECT" ]
+},
         {
             name: PROXY_GROUPS.MANUAL,
             icon: "https://gcore.jsdelivr.net/gh/shindgewongxj/WHATSINStash@master/icon/select.png",
