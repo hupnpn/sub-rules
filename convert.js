b664284fdd3aab89ad839951212dd7ac756bcb28
hupnpn <89003940+hupnpn@users.noreply.github.com>
2026-05-01 05:06:13 +0800

    Update convert.js

--- convert.js
+++ convert.js
@@ -841,7 +841,7 @@ function buildProxyGroups({
 
 // eslint-disable-next-line no-unused-vars -- 通过 vm.runInContext 在 yaml_generator 中被调用
 function main(config) {
-    const excludePattern = /官网|升级/i; 
+    const excludePattern = /官网|订阅|过期|到期|升级/i; 
     if (config.proxies) {
         config.proxies = config.proxies.filter(proxy => !excludePattern.test(proxy.name));
     }
